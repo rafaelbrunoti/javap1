de728a0734f07bf3dd25d5c3a3fbd76d750f8e8e
Sillas Leonardo <sillasleonardo@hotmail.com>
2013-08-10 12:25:02 -0300

    Servlets atualizado

--- src/servlets/GameServlet.java
+++ src/servlets/GameServlet.java
@@ -1,13 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import controllers.ExampleController;
+//import controller.Controller;
+//import controllers.ExampleController;
 
 
 @WebServlet("/GameServlet")
@@ -20,16 +23,26 @@ public class GameServlet extends HttpServlet {
     
     protected void execute (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
-    	ExampleController control = new ExampleController();
+//    	ExampleController control = new ExampleController();
+//    	
+//    	control.request = req;
+//    	control.response = resp;
+//    	control.save();
+//    	
+//    	try{
+//    		ExampleController control = new ExampleController();
+//			control.list();
+//			RequestDispatcher rd = request
+//					.getRequestDispatcher("/views/pessoa/pessoa.jsp");
+//			rd.forward(request, response);
+//		}
+//		catch(Exception e){
+//			System.out.println(e.getMessage());
+//		}
+	}
     	
-    	control.request = req;
-    	control.response = resp;
-    	control.save();
     	
-    	
-    	
-    	
-    }
+ 
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
