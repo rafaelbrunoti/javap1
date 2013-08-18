343f5914d5437497a3ea4ee5458bf473bb219d07
Juvenal Batista <jfbatista@hotmail.com>
2013-08-18 19:37:42 -0300

    Atualização para solucionar problema de loop na pagina principal.
    Como sempre cagada dos supostos programadores....kkkkkkk :)

--- src/controllers/ViewController.java
+++ src/controllers/ViewController.java
@@ -17,7 +17,7 @@ public class ViewController {
 		if (this.view == null || this.view == "" || this.view.isEmpty()) {
 			
 			System.out.print("Nenhum parametro de pagina encontrado, setando pagina padrão | ");
-			this.setView("/views/index.jsp");
+			this.setView("/views/game/lista.jsp");
 			
 		} else {
 			if (this.verifyPage(this.view)) {
@@ -36,7 +36,6 @@ public class ViewController {
 
 	public void setArrayPages() {
 
-		this.pages.add("/views/index");
 		this.pages.add("/game/lista");
 		this.pages.add("/game/cadastro");
 		this.pages.add("/genero/lista");
