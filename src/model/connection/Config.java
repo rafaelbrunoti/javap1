f7f1276080a6ef36c9dc4cc1d4d8473d2606dce8
Sillas Leonardo <sillasleonardo@hotmail.com>
2013-08-24 10:24:00 -0300

    add

--- src/model/connection/Config.java
+++ src/model/connection/Config.java
@@ -6,7 +6,7 @@ public class Config {
 	
 	public static String driver_development = "mysql";
 	public static String user_development = "root";
-	public static String pass_development = "";
+	public static String pass_development = "dimob#9100";
 	public static String host_development = "localhost";
 	public static String db_development = "dbgames";
 
